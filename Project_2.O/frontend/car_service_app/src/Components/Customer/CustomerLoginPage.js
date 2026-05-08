2d2898dc4da97e9811003b644a1bc8cf0bb22081
Riddhi022002 <shinderiddhi123@gmail.com>
2026-05-08 19:03:01 +0530

    fixed login error

--- Project_2.O/frontend/car_service_app/src/Components/Customer/CustomerLoginPage.js
+++ Project_2.O/frontend/car_service_app/src/Components/Customer/CustomerLoginPage.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import "../../StyleSheets/SharedCSS/login.css"
+// import "../../StyleSheets/SharedCSS/login.css"
+import "../../StyleSheets/Customer/CustomerLogin.css";
 
 const CustomerLogin = () => {
   const navigate = useNavigate();
